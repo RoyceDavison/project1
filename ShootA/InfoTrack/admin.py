96905e63345a4908858083b5774b61f6621aade4
fdong081 <fenfangdong@gmail.com>
2017-10-21 15:10:25 -0400

    Update admin.py

--- ShootA/InfoTrack/admin.py
+++ ShootA/InfoTrack/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import User, Post, Comment, Video, Picture, Picture_post, Picture_comment, Video_post, Video_comment
+
+#admin.site.register(User)
+admin.site.register(Comment)
+admin.site.register(Video)
+admin.site.register(Picture)
+admin.site.register(Picture_post)
+admin.site.register(Picture_comment)
+admin.site.register(Video_post)
+admin.site.register(Video_comment)
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CommentInline]
+
+
+
+class PostInline(admin.TabularInline):
+    model = Post
+
+@admin.register(User)
+class Userdmin(admin.ModelAdmin):
+    inlines = [PostInline]
